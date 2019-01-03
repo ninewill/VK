f36d9fa78abb594c5f6be37a7b95c746d93beaf2
ninewill <wtony1239271@gmail.com>
2019-01-03 17:06:49 +0800

    add gulp
    Add gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,37 @@
-//載入套件
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+//(1)
+var browserSync = require('browser-sync').create();
+var reload = browserSync.reload;
 
-
-//任務
 gulp.task('sass', function () {
-  return gulp.src('./sass/*.scss') //來源檔案
+  return gulp.src('./sass/*.scss')
     .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./css'));//目的地檔案
+    .pipe(gulp.dest('./css'));
+});
+// (2)
+// gulp.task('watch' ,function(){
+//     gulp.watch(['./sass/*.scss' ,'./sass/**/*.scss'], ['sass']);
+// });
+
+// (3)瀏覽器同步
+gulp.task('default', ['sass'], function () {
+
+  browserSync.init({
+    server: {
+      //根目錄
+      baseDir: "./",
+      index: "index.html"
+    }
+  });
+
+  gulp.watch(["sass/*.scss", "sass/**/*.scss"], ['sass']).on('change', reload);
+  gulp.watch("*.html").on('change', reload);
+  gulp.watch("js/*.js").on('change', reload);
+  // gulp.watch("images/*").on('change', reload);
 });
 
-gulp.task('watch', function () {
-  gulp.watch('./sass/*.scss', ['sass']);
-});
 
-gulp.task('default', ['sass', 'watch']);
+
+//(4)
+// gulp.task('default' ,['sass' , 'watch']);
